95c103796f8a66678c8fc02dbecb7f18c19bfc86
riveroo19 <114861335+riveroo19@users.noreply.github.com>
2023-03-03 12:39:30 +0100

    feat: initial structure
    Inicio del proyecto y prueba de push desde github Desktop

---
+++ baskotdrippy/src/main/java/com/practicaDWS/baskotdrippy/BaskotdrippyApplication.java
@@ -0,0 +1,13 @@
+package com.practicaDWS.baskotdrippy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaskotdrippyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaskotdrippyApplication.class, args);
+	}
+
+}
