7d920f59dc9b589aa0291fc76739c3cf0c1d3bf2
riveroo19 <114861335+riveroo19@users.noreply.github.com>
2023-03-17 17:55:01 +0100

    feat: starting UserControllers

---
+++ baskotdrippy/src/main/java/com/practicaDWS/baskotdrippy/controllers/UserController.java
@@ -0,0 +1,44 @@
+package com.practicaDWS.baskotdrippy.controllers;
+
+import com.practicaDWS.baskotdrippy.entities.User;
+import com.practicaDWS.baskotdrippy.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/users")
+    public String getUsers(Model model){
+
+        model.addAttribute("users", this.userService.getUsers());
+
+        return "prueba1";
+    }
+
+    @GetMapping("/users/{username}") //show any user details
+    public String getUserById(Model model, @PathVariable("username") String username){
+        User user = this.userService.getUserById(username);
+        if (user==null){
+            return "redirect:/error.html";
+        }
+        //model.addAttribute("user", user);
+        return "redirect:/contacts.html";
+    }
+
+    @GetMapping("/users/deleteUser/{username}")
+    public String deleteUser(Model model, @PathVariable("username") String username){
+        User user = this.userService.deleteUser(username);
+        if (user==null){
+            return "error.html";
+        }
+        return "deletionSuccess";
+    }
+
+}
