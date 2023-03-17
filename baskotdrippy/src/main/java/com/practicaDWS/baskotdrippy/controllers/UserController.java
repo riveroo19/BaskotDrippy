479f2b2cb708417a6e698d6e781857bf621248c8
riveroo19 <114861335+riveroo19@users.noreply.github.com>
2023-03-17 22:55:09 +0100

    keeping

--- baskotdrippy/src/main/java/com/practicaDWS/baskotdrippy/controllers/UserController.java
+++ baskotdrippy/src/main/java/com/practicaDWS/baskotdrippy/controllers/UserController.java
@@ -6,14 +6,23 @@ import com.practicaDWS.baskotdrippy.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserController {
 
     @Autowired
     UserService userService;
+
+    //pseudo login-related
+    @PostMapping("/login")
+    public String pseudoLogin(Model model, @RequestParam("username") String username){
+        User user = this.userService.getUserById(username);
+        if (user==null){
+            return "notRegistered";
+        }
+        return "redirect:/users/{username}";
+    }
 
     @GetMapping("/users")
     public String getUsers(Model model){
@@ -31,16 +40,27 @@ public class UserController {
         }
         model.addAttribute("user", user);
         model.addAttribute("outfits", user.getCreatedOutfits().values());
-        return "prueba1";
+        return "showUser";
     }
 
     @GetMapping("/users/deleteUser/{username}")
     public String deleteUser(@PathVariable("username") String username){
         User user = this.userService.deleteUser(username);
         if (user==null){
-            return "error.html";
+            return "redirect:/error.html";
         }
         return "deletionSuccess";
+    }
+
+    @GetMapping("/users/createUser") //redirected here when press register button
+    public String createUser(@RequestParam("username") String username, @RequestParam("fullname") String fullname,
+         @RequestParam("email") String email,  @RequestParam("bio") String bio,  @RequestParam("password") String password /*confirmpass*/){
+        User user = new User(username,fullname,bio,password,email);
+        user = this.userService.createUser(user);
+        if (user==null){
+            return "redirect:error";
+        }
+        return "redirect:/users/{username}";
     }
 
 }
