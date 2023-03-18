ee0830207bba3ddf7843161f1ebeac74d22f5d51
riveroo19 <114861335+riveroo19@users.noreply.github.com>
2023-03-18 03:08:46 +0100

    feat: keep going

--- baskotdrippy/src/main/java/com/practicaDWS/baskotdrippy/controllers/UserController.java
+++ baskotdrippy/src/main/java/com/practicaDWS/baskotdrippy/controllers/UserController.java
@@ -14,15 +14,23 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    //pseudo login-related
-    @PostMapping("/login")
-    public String pseudoLogin(Model model, @RequestParam("username") String username){
-        User user = this.userService.getUserById(username);
-        if (user==null){
-            return "notRegistered";
-        }
-        return "redirect:/users/{username}";
+    //extra functionalities
+    @GetMapping("/error")
+    public String error(){
+        return "error";
     }
+
+    @GetMapping("/login")
+    public String pseudoLogin(Model model, @RequestParam("username") String username, @RequestParam("password") String password){
+        User user = this.userService.getUserById(username);
+        if (user==null || !user.getPassword().equals(password)){
+            return "redirect:/error";
+            //return "notRegistered";
+        }
+        return this.getUserById(model, username);
+    }
+
+    //user functionalities
 
     @GetMapping("/users")
     public String getUsers(Model model){
@@ -36,31 +44,32 @@ public class UserController {
     public String getUserById(Model model, @PathVariable("username") String username){
         User user = this.userService.getUserById(username);
         if (user==null){
-            return "redirect:/error.html";
+            return "redirect:/error";
         }
         model.addAttribute("user", user);
         model.addAttribute("outfits", user.getCreatedOutfits().values());
-        return "showUser";
+        return "prueba1";
     }
 
     @GetMapping("/users/deleteUser/{username}")
     public String deleteUser(@PathVariable("username") String username){
         User user = this.userService.deleteUser(username);
         if (user==null){
-            return "redirect:/error.html";
+            return "redirect:/error";
         }
-        return "deletionSuccess";
+        return "redirect:deletionSuccess";
     }
 
-    @GetMapping("/users/createUser") //redirected here when press register button
-    public String createUser(@RequestParam("username") String username, @RequestParam("fullname") String fullname,
-         @RequestParam("email") String email,  @RequestParam("bio") String bio,  @RequestParam("password") String password /*confirmpass*/){
-        User user = new User(username,fullname,bio,password,email);
-        user = this.userService.createUser(user);
+    @PostMapping("/users/createUser") //redirected here when press register button
+    public String createUser(@RequestParam("username") String username, @RequestParam("email") String email,
+         @RequestParam("fullname") String fullname,  @RequestParam("bio") String bio,  @RequestParam("password") String password, @RequestParam("confirm") String confirm){
+        User user = this.userService.createUser(new User(username,fullname,bio,password,email));
         if (user==null){
-            return "redirect:error";
+            return "redirect:/error";
         }
-        return "redirect:/users/{username}";
+        return "redirect:/users";
     }
+
+
 
 }
